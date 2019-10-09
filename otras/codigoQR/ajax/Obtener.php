298e97caee3c4a7c46750f0f4b18288b01b292b5
EdwinQuintero01246 <47549545+EdwinQuintero01246@users.noreply.github.com>
2019-10-09 03:13:44 -0600

    Add files via upload

--- otras/codigoQR/ajax/Obtener.php
+++ otras/codigoQR/ajax/Obtener.php
@@ -1,9 +1,12 @@
 <?php
     require ('../../codigoQR/phpqrcode/qrlib.php');
     $var=time();
+    $dir01 = "../temp";
     $dir = "../temp/".$var;
+    if(!file_exists($dir01)){
+        mkdir($dir01);
+    }
         if(!file_exists($dir)){
-            
             mkdir($dir);
         }
         $filename = $dir.'test.png';
